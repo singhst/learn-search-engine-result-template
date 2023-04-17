57c8674ecce51ba4593eae8d844ccd28c3f6b1b9
st <71545537+singhst@users.noreply.github.com>
2023-04-18 00:16:53 +0800

    Delete test-sqlite.py

--- app/test-sqlite.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('search_engine.db')
-
-cur = conn.cursor()
-cursor = cur.execute("SELECT version_num from alembic_version")
-for row in cursor:
-    print("version_num = ", row[0])
-conn.close()
