b99df389e552a7790da284298b084b8d67b470fa
st <71545537+singhst@users.noreply.github.com>
2023-04-18 00:17:32 +0800

    Update test-sqlitedict.py

--- app/test-sqlitedict.py
+++ app/test-sqlitedict.py
@@ -26,36 +26,3 @@ queries = ["comput", "scienc"]
 # queries = ["test", "page"]
 
 print(invertedIndex[1])
-
-# title_score_dict = {}
-# title_id = -1
-# for query in queries:
-#     if query in title2TitleID.keys():
-#         title_id = title2TitleID[query]
-#     else:
-#         print("Title word not indexed:", query)
-#         continue
-#     posting_list = invertedIndexTitle[title_id]
-#     for document in posting_list:
-#         doc_id = document[0]
-#         # Pre-computed tf-idf
-#         tf_idf = document[1]
-#         # Accumulate the inner product
-#         if doc_id not in title_score_dict:
-#             title_score_dict[doc_id] = [tf_idf * 1, 1]
-#         else:
-#             title_score_dict[doc_id][0] += tf_idf * 1
-#             title_score_dict[doc_id][1] += 1
-# print(title_score_dict)
-
-
-# cos_sim_title = title_score_dict.copy()
-# for doc_id, score in title_score_dict.items():
-#     print(">>> doc_id: {}; score: {}".format(doc_id, score))
-#     word_list = forwardIndexTitle[doc_id]
-#     print(">>> word_list: {}".format(word_list))
-#     title_norm = titleNorm[doc_id]
-#     query_norm = math.sqrt(title_score_dict[doc_id][1])
-#     inner_prod = float(title_score_dict[doc_id][0])
-#     cos_sim_title[doc_id] = inner_prod / (title_norm * query_norm)
-# print("(1)>>> cos_sim_title:", cos_sim_title)
