e53120a71616db7a5d35682cd57deff9110ef9ed
st <71545537+singhst@users.noreply.github.com>
2023-05-07 19:41:58 +0800

    Update retrieve.py

--- app/search/retrieve.py
+++ app/search/retrieve.py
@@ -1,5 +1,5 @@
 from typing import List
-from app.search.scripts import similarity_query, sentence_transformer_query
+from app.search.scripts import sentence_transformer_query
 from pathlib import Path
 
 BASE_PATH = Path(__file__).resolve().parent
@@ -13,22 +13,11 @@ def result(query: List[str]):
     return query_results
 
 
-# def result(query: List[str]):
-#     # Split double quotes phrases
-#     queries = similarity_query.split_query_quot(query)
-#     # Clean, stem ...
-#     queries = similarity_query.clean_stem_query(queries)
-#     print(">>> retrieve.py | result() | Processed query: `{}`".format(queries))
-    
-#     print(">>> retrieve.py | result() | Retriving pages ...")
-#     query_results = similarity_query.retrive_func(queries)
-#     return query_results
-
-
 def similar(page_id: str):
-    queries = similarity_query.getTop5_FreqWord(page_id)
+    queries = sentence_transformer_query.getTop5_FreqWord(page_id)
     print(">>> retrieve.py | similar() | getTop5_FreqWord() query: `{}`".format(queries))
     
     print(">>> retrieve.py | similar() | Retriving pages ...")
-    query_results = similarity_query.retrive_func(queries)
+    query_results = sentence_transformer_query.process_query(" ".join(queries)
+)
     return query_results, queries
